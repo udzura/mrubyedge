aa1923f172481a14a21f21896979983536e9c079
Uchio Kondo <udzura@udzura.jp>
2024-05-15 01:50:56 +0900

    Bump version

--- mec/src/main.rs
+++ mec/src/main.rs
@@ -3,7 +3,7 @@
 extern crate bpaf;
 extern crate rand;
 
-const MRUBY_EDGE_DEFAULT_VERSION: &'static str = "0.1.5";
+const MRUBY_EDGE_DEFAULT_VERSION: &'static str = "0.1.6";
 
 use std::{fs::File, io::Read, path::PathBuf, process::Command};
 

f1a94cd9d216d219d30138caec8c8239615fcc96
Uchio Kondo <udzura@udzura.jp>
2025-11-23 22:57:02 +0900

    Add run subcommand

--- mrubyedge-cli/src/main.rs
+++ mrubyedge-cli/src/main.rs
@@ -6,15 +6,20 @@ mod subcommands;
 #[command(name = "mrbedge")]
 #[command(about = "mruby/edge command line interface", long_about = None)]
 #[command(version)]
+#[command(args_conflicts_with_subcommands = true)]
 struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
+
+    #[command(flatten)]
+    run_args: Option<subcommands::run::RunArgs>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Run Ruby code or binary
-    Run,
+    /// Run Ruby code or binary.
+    /// Run is invoked when rb/mrb file is directly passed to the command
+    Run(subcommands::run::RunArgs),
     /// Generate WebAssembly binary from Ruby code
     Wasm,
     /// Compile Ruby script to mrb
@@ -36,8 +41,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
     let cli = Cli::parse();
 
     match cli.command {
-        Some(Commands::Run) | None => {
-            subcommands::run::execute();
+        Some(Commands::Run(args)) => {
+            subcommands::run::execute(args)?;
         }
         Some(Commands::Wasm) => {
             subcommands::wasm::execute();
@@ -50,6 +55,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>>  {
                 subcommands::scaffold::execute_npm();
             }
         },
+        None => {
+            if let Some(args) = cli.run_args {
+                subcommands::run::execute(args)?;
+            } else {
+                eprintln!("No subcommand was used. Use --help for more information.");
+                std::process::exit(1);
+            }
+        }
     }
     Ok(())
 }
